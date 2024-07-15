9dd5afea0e1241af52707f1819a21e42bfa421db
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-15 16:19:28 +0530

    chore: Refactor Cat component to improve eye interaction

--- src/Cat.jsx
+++ src/Cat.jsx
@@ -3,51 +3,61 @@ import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 const Cat = () => {
-  const catRef = useRef();
   const leftEyeRef = useRef();
   const rightEyeRef = useRef();
+  const leftPupilRef = useRef();
+  const rightPupilRef = useRef();
+  const cursor = new THREE.Vector2();
+  const raycaster = new THREE.Raycaster();
 
   useEffect(() => {
     const handleMouseMove = (event) => {
-      const x = (event.clientX / window.innerWidth) * 2 - 1;
-      const y = -(event.clientY / window.innerHeight) * 2 + 1;
-      const vector = new THREE.Vector3(x, y, 0.5).unproject(catRef.current);
-      const dir = vector.sub(catRef.current.position).normalize();
-      const distance = -catRef.current.position.z / dir.z;
-      const pos = catRef.current.position
-        .clone()
-        .add(dir.multiplyScalar(distance));
-
-      leftEyeRef.current.lookAt(pos);
-      rightEyeRef.current.lookAt(pos);
+      cursor.x = (event.clientX / window.innerWidth) * 2 - 1;
+      cursor.y = -(event.clientY / window.innerHeight) * 2 + 1;
     };
 
     window.addEventListener("mousemove", handleMouseMove);
     return () => window.removeEventListener("mousemove", handleMouseMove);
   }, []);
 
+  useFrame(({ camera }) => {
+    raycaster.setFromCamera(cursor, camera);
+    const intersectPoint = new THREE.Vector3();
+    raycaster.ray.at(10, intersectPoint);
+
+    leftPupilRef.current.lookAt(intersectPoint);
+    rightPupilRef.current.lookAt(intersectPoint);
+  });
+
   return (
-    <group ref={catRef}>
+    <group>
+      {/* Cat's Head */}
       <mesh position={[0, 1, 0]}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshStandardMaterial color="orange" />
       </mesh>
-      <mesh ref={leftEyeRef} position={[-0.3, 1.3, 0.8]}>
-        <sphereGeometry args={[0.1, 32, 32]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      <mesh ref={rightEyeRef} position={[0.3, 1.3, 0.8]}>
-        <sphereGeometry args={[0.1, 32, 32]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      <mesh position={[-0.3, 1.3, 0.9]}>
-        <sphereGeometry args={[0.05, 32, 32]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
-      <mesh position={[0.3, 1.3, 0.9]}>
-        <sphereGeometry args={[0.05, 32, 32]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
+      {/* Left Eye */}
+      <group ref={leftEyeRef} position={[-0.3, 1.3, 0.8]}>
+        <mesh>
+          <sphereGeometry args={[0.15, 32, 32]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        <mesh ref={leftPupilRef} position={[0, 0, 0.11]}>
+          <sphereGeometry args={[0.05, 32, 32]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+      </group>
+      {/* Right Eye */}
+      <group ref={rightEyeRef} position={[0.3, 1.3, 0.8]}>
+        <mesh>
+          <sphereGeometry args={[0.15, 32, 32]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        <mesh ref={rightPupilRef} position={[0, 0, 0.11]}>
+          <sphereGeometry args={[0.05, 32, 32]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+      </group>
     </group>
   );
 };
