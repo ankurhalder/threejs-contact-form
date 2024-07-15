1ecd437398f09277854815fef8062e521b4c60c4
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-15 16:23:06 +0530

    refactor: Improve eye interaction in Cat component

--- src/Cat.jsx
+++ src/Cat.jsx
@@ -3,12 +3,10 @@ import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 const Cat = () => {
-  const leftEyeRef = useRef();
-  const rightEyeRef = useRef();
   const leftPupilRef = useRef();
   const rightPupilRef = useRef();
   const cursor = new THREE.Vector2();
-  const raycaster = new THREE.Raycaster();
+  const maxPupilOffset = 0.05; // Maximum offset for the pupils
 
   useEffect(() => {
     const handleMouseMove = (event) => {
@@ -21,12 +19,46 @@ const Cat = () => {
   }, []);
 
   useFrame(({ camera }) => {
-    raycaster.setFromCamera(cursor, camera);
-    const intersectPoint = new THREE.Vector3();
-    raycaster.ray.at(10, intersectPoint);
+    const vector = new THREE.Vector3(cursor.x, cursor.y, 0.5).unproject(camera);
+    const dir = vector.sub(camera.position).normalize();
+    const distance = -camera.position.z / dir.z;
+    const pos = camera.position.clone().add(dir.multiplyScalar(distance));
 
-    leftPupilRef.current.lookAt(intersectPoint);
-    rightPupilRef.current.lookAt(intersectPoint);
+    const leftEyePos = new THREE.Vector3(-0.3, 1.3, 0.8);
+    const rightEyePos = new THREE.Vector3(0.3, 1.3, 0.8);
+
+    const leftPupilOffset = new THREE.Vector3(
+      THREE.MathUtils.clamp(
+        (pos.x - leftEyePos.x) * 0.1,
+        -maxPupilOffset,
+        maxPupilOffset
+      ),
+      THREE.MathUtils.clamp(
+        (pos.y - leftEyePos.y) * 0.1,
+        -maxPupilOffset,
+        maxPupilOffset
+      ),
+      0.1 // Slightly in front of the eye
+    );
+
+    const rightPupilOffset = new THREE.Vector3(
+      THREE.MathUtils.clamp(
+        (pos.x - rightEyePos.x) * 0.1,
+        -maxPupilOffset,
+        maxPupilOffset
+      ),
+      THREE.MathUtils.clamp(
+        (pos.y - rightEyePos.y) * 0.1,
+        -maxPupilOffset,
+        maxPupilOffset
+      ),
+      0.1 // Slightly in front of the eye
+    );
+
+    leftPupilRef.current.position.copy(leftEyePos.clone().add(leftPupilOffset));
+    rightPupilRef.current.position.copy(
+      rightEyePos.clone().add(rightPupilOffset)
+    );
   });
 
   return (
@@ -37,23 +69,23 @@ const Cat = () => {
         <meshStandardMaterial color="orange" />
       </mesh>
       {/* Left Eye */}
-      <group ref={leftEyeRef} position={[-0.3, 1.3, 0.8]}>
+      <group position={[-0.3, 1.3, 0.8]}>
         <mesh>
           <sphereGeometry args={[0.15, 32, 32]} />
           <meshStandardMaterial color="white" />
         </mesh>
-        <mesh ref={leftPupilRef} position={[0, 0, 0.11]}>
+        <mesh ref={leftPupilRef} position={[0, 0, 0.1]}>
           <sphereGeometry args={[0.05, 32, 32]} />
           <meshStandardMaterial color="black" />
         </mesh>
       </group>
       {/* Right Eye */}
-      <group ref={rightEyeRef} position={[0.3, 1.3, 0.8]}>
+      <group position={[0.3, 1.3, 0.8]}>
         <mesh>
           <sphereGeometry args={[0.15, 32, 32]} />
           <meshStandardMaterial color="white" />
         </mesh>
-        <mesh ref={rightPupilRef} position={[0, 0, 0.11]}>
+        <mesh ref={rightPupilRef} position={[0, 0, 0.1]}>
           <sphereGeometry args={[0.05, 32, 32]} />
           <meshStandardMaterial color="black" />
         </mesh>
