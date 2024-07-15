4749e223fc0faddeec838beda413de45a2302baf
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-15 16:58:59 +0530

    refactor: Remove nose, ears, and whiskers from Cat component

--- src/Cat.jsx
+++ src/Cat.jsx
@@ -8,9 +8,6 @@ const Cat = () => {
   const leftPupilRef = useRef();
   const rightPupilRef = useRef();
   const mouthRef = useRef();
-  const noseRef = useRef();
-  const earLeftRef = useRef();
-  const earRightRef = useRef();
   const cursor = new THREE.Vector2();
   const raycaster = new THREE.Raycaster();
 
@@ -39,11 +36,6 @@ const Cat = () => {
       <mesh position={[0, 1, 0]}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshStandardMaterial color="orange" roughness={0.6} metalness={0.1} />
-      </mesh>
-      {/* Nose */}
-      <mesh ref={noseRef} position={[0, 0.8, 1.1]}>
-        <coneGeometry args={[0.05, 0.2, 32]} />
-        <meshStandardMaterial color="black" />
       </mesh>
       {/* Mouth */}
       <mesh ref={mouthRef} position={[0, 0.7, 0.9]}>
@@ -74,56 +66,6 @@ const Cat = () => {
         <sphereGeometry args={[0.05, 32, 32]} />
         <meshStandardMaterial color="black" />
       </mesh>
-      {/* Ears */}
-      <group>
-        {/* Left Ear */}
-        <mesh ref={earLeftRef} position={[-0.8, 1.5, 0]}>
-          <coneGeometry args={[0.3, 1, 32]} />
-          <meshStandardMaterial
-            color="orange"
-            roughness={0.6}
-            metalness={0.1}
-          />
-        </mesh>
-        {/* Right Ear */}
-        <mesh ref={earRightRef} position={[0.8, 1.5, 0]}>
-          <coneGeometry args={[0.3, 1, 32]} />
-          <meshStandardMaterial
-            color="orange"
-            roughness={0.6}
-            metalness={0.1}
-          />
-        </mesh>
-      </group>
-      {/* Whiskers */}
-      <group>
-        {/* Left Whiskers */}
-        <mesh position={[-0.1, 1.1, 1.05]}>
-          <planeGeometry args={[0.5, 0.01]} />
-          <meshStandardMaterial color="black" />
-        </mesh>
-        <mesh position={[-0.1, 1.1, 1.05]}>
-          <planeGeometry args={[0.5, 0.01]} />
-          <meshStandardMaterial color="black" rotation={[0, 0, Math.PI / 4]} />
-        </mesh>
-        <mesh position={[-0.1, 1.1, 1.05]}>
-          <planeGeometry args={[0.5, 0.01]} />
-          <meshStandardMaterial color="black" rotation={[0, 0, -Math.PI / 4]} />
-        </mesh>
-        {/* Right Whiskers */}
-        <mesh position={[0.1, 1.1, 1.05]}>
-          <planeGeometry args={[0.5, 0.01]} />
-          <meshStandardMaterial color="black" />
-        </mesh>
-        <mesh position={[0.1, 1.1, 1.05]}>
-          <planeGeometry args={[0.5, 0.01]} />
-          <meshStandardMaterial color="black" rotation={[0, 0, Math.PI / 4]} />
-        </mesh>
-        <mesh position={[0.1, 1.1, 1.05]}>
-          <planeGeometry args={[0.5, 0.01]} />
-          <meshStandardMaterial color="black" rotation={[0, 0, -Math.PI / 4]} />
-        </mesh>
-      </group>
       {/* Tail */}
       <mesh position={[0, 0.5, -1]}>
         <cylinderGeometry args={[0.05, 0.1, 1, 16]} />
