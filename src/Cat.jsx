6c3f313da71a3048097b9c3e60e9dd95159f72a7
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-15 17:23:03 +0530

    refactor: Improve eye interaction in Cat component

--- src/Cat.jsx
+++ src/Cat.jsx
@@ -3,11 +3,8 @@ import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 const Cat = () => {
-  const leftEyeRef = useRef();
-  const rightEyeRef = useRef();
   const leftPupilRef = useRef();
   const rightPupilRef = useRef();
-  const mouthRef = useRef();
   const cursor = new THREE.Vector2();
   const raycaster = new THREE.Raycaster();
 
@@ -15,6 +12,8 @@ const Cat = () => {
     const handleMouseMove = (event) => {
       cursor.x = (event.clientX / window.innerWidth) * 2 - 1;
       cursor.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      console.log("Cursor position:", cursor.x, cursor.y);
     };
 
     window.addEventListener("mousemove", handleMouseMove);
@@ -26,8 +25,12 @@ const Cat = () => {
     const intersectPoint = new THREE.Vector3();
     raycaster.ray.at(10, intersectPoint);
 
-    leftPupilRef.current.lookAt(intersectPoint);
-    rightPupilRef.current.lookAt(intersectPoint);
+    console.log("Intersection point:", intersectPoint);
+
+    if (leftPupilRef.current && rightPupilRef.current) {
+      leftPupilRef.current.lookAt(intersectPoint);
+      rightPupilRef.current.lookAt(intersectPoint);
+    }
   });
 
   return (
@@ -38,34 +41,34 @@ const Cat = () => {
         <meshStandardMaterial color="orange" roughness={0.6} metalness={0.1} />
       </mesh>
       {/* Mouth */}
-      <mesh ref={mouthRef} position={[0, 0.7, 0.9]}>
+      <mesh position={[0, 0.7, 0.9]}>
         <boxGeometry args={[0.2, 0.05, 0.1]} />
         <meshStandardMaterial color="black" />
       </mesh>
       {/* Left Eye */}
-      <group ref={leftEyeRef} position={[-0.3, 1.3, 0.8]}>
+      <group position={[-0.3, 1.3, 0.8]}>
         <mesh>
           <sphereGeometry args={[0.15, 32, 32]} />
           <meshStandardMaterial color="white" />
+        </mesh>
+        {/* Left Pupil */}
+        <mesh ref={leftPupilRef} position={[0, 0, 0.11]}>
+          <sphereGeometry args={[0.05, 32, 32]} />
+          <meshStandardMaterial color="black" />
         </mesh>
       </group>
       {/* Right Eye */}
-      <group ref={rightEyeRef} position={[0.3, 1.3, 0.8]}>
+      <group position={[0.3, 1.3, 0.8]}>
         <mesh>
           <sphereGeometry args={[0.15, 32, 32]} />
           <meshStandardMaterial color="white" />
         </mesh>
+        {/* Right Pupil */}
+        <mesh ref={rightPupilRef} position={[0, 0, 0.11]}>
+          <sphereGeometry args={[0.05, 32, 32]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
       </group>
-      {/* Left Pupil */}
-      <mesh ref={leftPupilRef} position={[-0.3, 1.3, 0.91]}>
-        <sphereGeometry args={[0.05, 32, 32]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
-      {/* Right Pupil */}
-      <mesh ref={rightPupilRef} position={[0.3, 1.3, 0.91]}>
-        <sphereGeometry args={[0.05, 32, 32]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
       {/* Tail */}
       <mesh position={[0, 0.5, -1]}>
         <cylinderGeometry args={[0.05, 0.1, 1, 16]} />
