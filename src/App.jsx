28d21daffbe8de520026400937bd0a111159139d
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-17 13:30:37 +0530

    refactor: Remove unused code and dependencies from Cat component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Cat from "./Cat";
-
-const App = () => {
-  return (
-    <Canvas style={{ height: "100vh", width: "100vw" }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 10, 5]} intensity={1} />
-      <Cat />
-      <OrbitControls />
-    </Canvas>
-  );
-};
+function App() {
+  return <div></div>;
+}
 
 export default App;
