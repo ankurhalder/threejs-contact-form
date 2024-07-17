49f9464d34172ad5220010de388c3a44b01a0d35
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-17 14:28:42 +0530

    chore: Remove unused event parameter in BreakingBad component

--- src/BreakingBad.jsx
+++ src/BreakingBad.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import * as THREE from "three";
 
@@ -116,11 +117,11 @@ const WalterWhiteScene = () => {
     }
   };
 
-  const mouseup = (e) => {
+  const mouseup = () => {
     isUp = false;
   };
 
-  const mousedown = (e) => {
+  const mousedown = () => {
     isUp = true;
   };
 
